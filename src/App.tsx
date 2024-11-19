cdf2bff4caf197f3b12f171c10f35e5ab6c78c78
GoBDaNWeb <bogdan.ognitsiy@yandex.ru>
2024-11-19 13:36:14 +0300

    test init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import "./app/styles/index.scss";
+
+import { Donation } from "./donation/Donation";
+function App() {
+  return <Donation />;
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import react from "@vitejs/plugin-react";
+
+//@ts-expect-error исправить
+import path from "path";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      //@ts-expect-error исправить
+      "@/app": path.resolve(__dirname, "src/app"),
+      //@ts-expect-error исправить
+      "@/pages": path.resolve(__dirname, "src/pages"),
+      //@ts-expect-error исправить
+      "@/widgets": path.resolve(__dirname, "src/widgets"),
+      //@ts-expect-error исправить
+      "@/features": path.resolve(__dirname, "src/features"),
+      //@ts-expect-error исправить
+      "@/entities": path.resolve(__dirname, "src/entities"),
+      //@ts-expect-error исправить
+      "@/shared": path.resolve(__dirname, "src/shared"),
+    },
+  },
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use 'src/app/styles/mixins' as *;
+				@use 'src/app/styles/variables' as *;`,
+      },
+    },
+  },
+});
